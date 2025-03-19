c67ebc7b2bca8f2aca36ad2430560c8e8e06d39a
laura nunez <laupao41@gmail.com>
2025-03-19 13:34:38 +0900

    initial commit

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+      // Ask grade percentage
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+        string sign = "";
+
+        // letter grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // (+ or -)
+        int lastDigit = grade % 10;
+        if (grade >= 60 && letter != "A" && letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        //  A+ and F grades
+        if (letter == "A" && grade >= 90 && grade < 93)
+        {
+            sign = "-";
+        }
+        else if (letter == "F")
+        {
+            sign = "";
+        }
+
+        // letter grade
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+
+        //  pass or fail
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You'll do better next time.");
+        }
+   
     }
 }

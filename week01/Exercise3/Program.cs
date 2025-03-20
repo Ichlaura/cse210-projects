f751f326a8be1bd28a692d26412c48bc2aeed732
laura nunez <laupao41@gmail.com>
2025-03-20 10:48:48 +0900

    initial commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+         // For Parts 1 and 2,
+          // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        
+           // For Part 3,
+         Random random = new Random();
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            int magicNumber = random.Next(1, 101); // Random number between 1 and 100
+            int guess = 0;
+            int attempts = 0;
+
+            Console.WriteLine("Welcome to the Guess My Number game!");
+            
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {attempts} attempts!");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+            playAgain = response == "yes";
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
+    
 }
